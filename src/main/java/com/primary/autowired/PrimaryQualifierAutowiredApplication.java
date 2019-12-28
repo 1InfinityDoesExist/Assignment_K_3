a81a6586b0665f5477c24c466a1a75a8b5fd23fb
avinash <avinash.patel@kuliza.com>
2019-12-28 11:26:47 +0530

    Pimary Qualifier Autowired

---
+++ src/main/java/com/primary/autowired/PrimaryQualifierAutowiredApplication.java
@@ -0,0 +1,13 @@
+package com.primary.autowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryQualifierAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimaryQualifierAutowiredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primary/autowired/PrimaryQualifierAutowiredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primary.autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryQualifierAutowiredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
